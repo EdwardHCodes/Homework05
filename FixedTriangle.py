05af6a557d833125e96742d60ca75e1087b48ec4
FastCashHash <38704768+FastCashHash@users.noreply.github.com>
2020-02-25 20:31:13 -0600

    Update FixedTriangle.py

--- FixedTriangle.py
+++ FixedTriangle.py
@@ -29,22 +29,22 @@ def classifyTriangle(a,b,c):
 
     # require that the input values be >= 0 and <= 200
     if a > 200 or b > 200 or c > 200:
-        return 'InvalidInput'
+        return 'Invalid Input'
         
     if a <= 0 or b <= 0 or c <= 0:
-        return 'InvalidInput'
+        return 'Invalid Input'
     
     # verify that all 3 inputs are integers  
     # Python's "isinstance(object,type) returns True if the object is of the specified type
     if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
+        return 'Invalid Input';
       
     # This information was not in the requirements spec but 
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
     if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
-        return 'NotATriangle'
+        return 'Not A Triangle'
         
     # now we know that we have a valid triangle 
     if a == b and b == a:
